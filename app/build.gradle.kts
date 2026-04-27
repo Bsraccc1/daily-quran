99447dbf0943afbc7dca8d79f0643f51789bcd37
QuranReaderDev <dev@quranreader.app>
2026-04-27 22:34:31 +0700

    chore: bump version to 3.0.1 (versionCode 4) for v3.0.1 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.quranreader.custom"
         minSdk = 21
         targetSdk = 34
-        versionCode = 3
-        versionName = "3.0.0"
+        versionCode = 4
+        versionName = "3.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
